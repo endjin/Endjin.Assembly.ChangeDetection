a4d36c4a04235f2737a3ca60290ff288f3783435
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2015-07-13 17:07:31 +0100

    Refactor for simplicity

--- Solutions/Endjin.Assembly.ChangeDetection/Rules/BreakingChangeRule.cs
+++ Solutions/Endjin.Assembly.ChangeDetection/Rules/BreakingChangeRule.cs
@@ -1,6 +1,12 @@
 ﻿namespace AssemblyDifferences.Rules
 {
+    #region Using Directives
+
+    using System.Linq;
+
     using AssemblyDifferences.Diff;
+
+    #endregion
 
     public class BreakingChangeRule : IRule
     {
@@ -18,49 +24,29 @@
                     if (typeChange.HasChangedBaseType)
                     {
                         return false;
-                        //this.Out.WriteLine("\t\tBase type changed: {0} -> {1}", typeChange.TypeV1.IsNotNull(() => typeChange.TypeV1.BaseType.IsNotNull(() => typeChange.TypeV1.BaseType.FullName)), typeChange.TypeV2.IsNotNull(() => typeChange.TypeV2.BaseType.IsNotNull(() => typeChange.TypeV2.BaseType.FullName)));
                     }
 
                     if (typeChange.Interfaces.Count > 0)
                     {
-                        foreach (var removedItd in typeChange.Interfaces.Removed)
+                        if (typeChange.Interfaces.Removed.Any())
                         {
                             return false;
-                            //this.Out.WriteLine("\t\t- interface: {0}", removedItd.ObjectV1.FullName);
                         }
                     }
 
-                    /*foreach (var addedEvent in typeChange.Events.Added)
-                    {
-                        //this.Out.WriteLine("\t\t+ {0}", addedEvent.ObjectV1.Print());
-                    }*/
-
-                    foreach (var remEvent in typeChange.Events.Removed)
+                    if (typeChange.Events.Removed.Any())
                     {
                         return false;
-                        //this.Out.WriteLine("\t\t- {0}", remEvent.ObjectV1.Print());
                     }
 
-                    /*foreach (var addedField in typeChange.Fields.Added)
-                    {
-                        //this.Out.WriteLine("\t\t+ {0}", addedField.ObjectV1.Print(FieldPrintOptions.All));
-                    }*/
-
-                    foreach (var remField in typeChange.Fields.Removed)
+                    if (typeChange.Fields.Removed.Any())
                     {
                         return false;
-                        //this.Out.WriteLine("\t\t- {0}", remField.ObjectV1.Print(FieldPrintOptions.All));
                     }
 
-                    /*foreach (var addedMethod in typeChange.Methods.Added)
-                    {
-                        //this.Out.WriteLine("\t\t+ {0}", addedMethod.ObjectV1.Print(MethodPrintOption.Full));
-                    }*/
-
-                    foreach (var remMethod in typeChange.Methods.Removed)
+                    if (typeChange.Methods.Removed.Any())
                     {
                         return false;
-                        //this.Out.WriteLine("\t\t- {0}", remMethod.ObjectV1.Print(MethodPrintOption.Full));
                     }
                 }
             }
